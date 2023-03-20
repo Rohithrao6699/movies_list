a2de6b15dfedd4302fa062d10cd13c3b1272f885
Rohith rao <107629715+Rohithrao6699@users.noreply.github.com>
2023-03-20 11:56:38 +0530

    Added_overviews_and_styles.

--- movies/src/Lists.js
+++ movies/src/Lists.js
@@ -1,14 +1,13 @@
 import { useEffect, useState } from 'react'
 import axios from "axios"
-import './Lists.css'
-
-
+import './Lists.css' 
 
 function List() {
  const [state, setState] = useState([])
  const [datas, setdatas] = useState([])
- const [overview, setOverview] = useState(' ')
+ const [selectedMovie, setSelectedMovie] = useState(null);
 
+ 
  async function getData() {
     try {
         const response =  await axios.get("https://api.themoviedb.org/3/trending/all/week?api_key=1cda66409fb42c9437bdefe0d4473be2");
@@ -22,7 +21,7 @@ useEffect(() => {
     getData();
    }, []);
 
-   useEffect(() => {
+useEffect(() => {
     console.log(state)
    }, [state]);
 
@@ -39,23 +38,28 @@ const handleclick = () => {
     // console.log(Randommovies())
 };
 
-const handleimageClick = (movieoverview, index) => {
-    setOverview(movieoverview)
-}
+const handleImageClick = (movie) => {
+    setSelectedMovie(movie);
+    console.log(movie)
+  };
 
 
 return(
     <div className='container'>
     <h1>Click to Add Movies and TV shows</h1>
-    <button className='button' onClick={handleclick}>Add Movies</button>
+    <button className='button' onClick={handleclick}>Add Movie</button>
+    <h4>Click on movie for overview!</h4>
     <div className="movie-grid">
     {datas.map((movie, index) => 
     <div key={index} className="movie-item">
-        <h2 className='title'>{movie.name || movie.title}</h2>
         <img src={`https://image.tmdb.org/t/p/w300${movie.backdrop_path}`} alt={`${movie.name || movie.title} backdrop`} 
         className='image'
-        onClick={() => handleimageClick(movie.overview, movie.index)}/>
-        <p>{overview}</p>
+        onClick={() => handleImageClick(movie)}
+            />
+        <h2 className='title'>{movie.name || movie.title}</h2>
+            {movie === selectedMovie && (
+              <p className="overview">{movie.overview}</p>
+            )}
     </div>
     )}
     </div>
